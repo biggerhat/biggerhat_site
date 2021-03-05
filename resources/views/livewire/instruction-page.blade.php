85e1c1ef2acd9b0465823231292ae61046d547fb
biggerhat <shutdownemergency@gmail.com>
2021-03-04 19:53:23 -0500

    feat: add instruction page

--- resources/views/livewire/instruction-page.blade.php
+++ resources/views/livewire/instruction-page.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="container mx-auto">
         <div class="block w-full h-full py-5 text-center">
-            <span class="text-4xl {{ $textColor }} lg:text-6xl md:text-4xl faction_header">
+            <span class="text-4xl text-gray-900 lg:text-6xl md:text-4xl faction_header">
                 {{ $mini->name }}
             </span>
         </div>
@@ -18,12 +18,8 @@
         @foreach ($instruction->minis as $miniListed)
             <div class="text-center">
                 <a href="{{ route('character.view', $miniListed->slug) }}"
-                class="inline-block text-center p-0.5 text-white rounded-full @if ($miniListed->id == $mini->id) bg-gray-900 @elseif (count($miniListed->factions)
-                    > 1) bg-gradient-to-r from-{{ $miniListed->factions[0]['bg_color'] }} to
-                {{ $miniListed->factions[1]['bg_color'] }} @else
-                    bg-{{ $miniListed->factions[0]['bg_color'] }} @endif px-2
-                    py-1
-                    font-bold mb-1 cursor-pointer">{{ strtoupper($miniListed->name) }}</a>
+                    class="inline-block text-center p-0.5 text-white rounded-full @if ($miniListed->id == $mini->id) bg-gray-600 @else bg-gray-900 @endif
+                    px-2 py-1 font-bold mb-1 cursor-pointer">{{ strtoupper($miniListed->name) }}</a>
             </div>
         @endforeach
 
